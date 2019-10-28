a88ee5ff06a035c909886d96d54a4846dfbfd2f6
Tomasz Cichociński <tomaszcichocinski@gmail.com>
2019-10-28 13:33:31 +0100

    fix: dump lib into git

--- src/mergeClasses.js
+++ src/mergeClasses.js
@@ -1,22 +1,22 @@
-import _ from 'lodash'
+import { forOwn, cloneDeep } from "lodash";
 
 export const mergeClasses = (classes, activeNames = []) => {
-  const styles = (classes.default && _.cloneDeep(classes.default)) || {}
-  activeNames.map((name) => {
-    const toMerge = classes[name]
+  const styles = (classes.default && cloneDeep(classes.default)) || {};
+  activeNames.map(name => {
+    const toMerge = classes[name];
     if (toMerge) {
-      _.forOwn(toMerge, (value, key) => {
+      forOwn(toMerge, (value, key) => {
         if (!styles[key]) {
-          styles[key] = {}
+          styles[key] = {};
         }
 
-        styles[key] = { ...styles[key], ...toMerge[key] }
-      })
+        styles[key] = { ...styles[key], ...toMerge[key] };
+      });
     }
 
-    return name
-  })
-  return styles
-}
+    return name;
+  });
+  return styles;
+};
 
-export default mergeClasses
+export default mergeClasses;
